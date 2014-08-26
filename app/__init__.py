056988635c31094480d1f1e511da2d197df7aa28
Bill Mei <billmei@users.noreply.github.com>
2014-08-26 14:00:08 -0400

    SASS and JS compiler

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.ext.assets import Environment, Bundle
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True, static_url_path='/static')
@@ -8,6 +9,15 @@ try:
     app.config.from_pyfile('config.py')
 except EnvironmentError:
     pass
+
+assets = Environment(app)
+scss = Bundle('sass/normalize.scss', 'sass/colors.scss', 'sass/main.scss',
+    filters='pyscss cssmin', output='gen/all.css')
+js = Bundle('js/jquery-1.10.2.min.js', 'js/modernizr-2.6.2.min.js', 'js/plugins.js',
+    'js/main.js',
+    filters='jsmin', output='gen/packed.js')
+assets.register('scss_all', scss)
+assets.register('js_all', js)
 
 db = SQLAlchemy(app)
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,36 @@
+from flask import render_template, send_from_directory
+from flask import url_for, redirect, request
 from app import app
 
+# Deletes trailing slashes before loading the page
+@app.before_request 
+def remove_trailing_slash(): 
+    if request.path != '/' and request.path.endswith('/'): 
+        return redirect(request.path[:-1], code=301) 
+
+# Redirects to the template renderer to strip .html extension
+@app.route('/<pagename>.html')
+def render_html_page(pagename):
+    return redirect(url_for('render_' + pagename + '_page'), code=301)
+
+# (Pseudo) static html pages
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+def render_index_page():
+    return render_template('index.html')
+
+# Error Handlers
+@app.errorhandler(400)
+def page_not_found(error):
+    return render_template('error.html', errorcode=400), 400
+@app.errorhandler(401)
+def page_not_found(error):
+    return render_template('error.html', errorcode=401), 401
+@app.errorhandler(403)
+def page_not_found(error):
+    return render_template('error.html', errorcode=403), 403
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html', errorcode=404), 404
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('error.html', errorcode=500), 500
